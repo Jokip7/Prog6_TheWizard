557e111d22597fa3f4aacdce78f2e7bcab288684
Chris Meesters <Jokip7>
2016-11-16 14:58:46 +0100

    uncomment

--- Wizard.Test/Wizard_Test.cs
+++ Wizard.Test/Wizard_Test.cs
@@ -3,23 +3,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Wizard.Test
 {
-    //[TestClass]
-    //public class Wizard_Test
-    //{
-    //    [TestMethod]
-    //    public void Foramisforameur_goed()
-    //    {
-    //        //1. Arrange
-    //        //2. Act
-    //        //3. Assert
-    //    }
+    [TestClass]
+    public class Wizard_Test
+    {
+        private Wizard.Tovenaar tovenaar;
+        private Wizard.Toverstaf staf;
 
-    //    [TestMethod]
-    //    public void Foramisforameur_fout()
-    //    {
-    //        //1. Arrange
-    //        //2. Act
-    //        //3. Assert
-    //    }
-    //}
+        [TestMethod]
+        public void Foramisforameur_goed()
+        {
+            //1. Arrange
+            //2. Act
+            //3. Assert
+        }
+
+        [TestMethod]
+        public void Foramisforameur_fout()
+        {
+            //1. Arrange
+            //2. Act
+            //3. Assert
+        }
+    }
 }
